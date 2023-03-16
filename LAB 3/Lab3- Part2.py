1d322d648344fa74bff2c43de85fa65adbadf656
modup <adeonim0705@student.bowiestate.edu>
2023-03-15 20:56:30 -0400

    Commit

---
+++ LAB 3/Lab3- Part2.py
@@ -0,0 +1,13 @@
+score1=input('Enter score for INS405')
+score2=input('Enter score for BUIS 360')
+score3=input('Enter score for DANL 470')
+
+sum=int(score1)+int(score2)+int(score3)
+average=int(sum)/3
+print(sum)
+print(average)
+
+
+
+
+
