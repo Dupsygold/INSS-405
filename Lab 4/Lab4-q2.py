c724e66a1979faf559ed794d2cc7c129ecd2a0eb
modup <adeonim0705@student.bowiestate.edu>
2023-03-29 19:37:53 -0400

    Commit

--- Lab 4/Lab4-q2.py
+++ Lab 4/Lab4-q2.py
@@ -3,12 +3,15 @@
 # cold = 0 - 30
 # Extreme_cold = <0
 
-Temperature = input("Temperature: ")
-if(int(Temperature)) >= 50:
-    print("Hot")
-elif(int(Temperature) <=50 and int(Temperature) >=30):
-    print("warm")
-elif(int(Temperature) <=30 and int(Temperature)>= 0):
-    print("cold")
-else:
-    print("Extreme cold")
+temperature = 5
+
+for i in range(temperature):
+    temperature = int(input("enter temperature: "))
+    if(int(temperature)) >= 50:
+        print("Hot")
+    elif(int(temperature) <=30 and int(temperature) >=50):
+        print("warm")
+    elif(int(temperature) >=0 and int(temperature)<= 30):
+        print("cold")
+    elif(int(temperature) <0):
+        print("Extreme cold")
