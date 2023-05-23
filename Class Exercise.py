3f1dcb1918f21b36bacab6d88905c6cc739c9b7d
modup <adeonim0705@student.bowiestate.edu>
2023-05-23 09:38:14 -0400

    Commit

---
+++ Class Exercise.py
@@ -0,0 +1,18 @@
+def request():
+     num=input('Enter variable num1')
+     num2=input('Enter variable num2')
+     print(addition(num1,num2))
+
+      def addition(num1.num2):
+     sum=int(num1)+int(num2)
+     return sum
+
+def subtraction(num1,num2):
+    difference=int(num1)-int(num2)
+    return difference
+
+def multiplication(num1,num2):
+    multi=int(num1)*int(num2)
+    return
+
+
